20cf735ab7d650db37ace1c72b5281b42e6e3137
Yousof Wakili <ywakili18@gmail.com>
2021-11-22 12:39:05 -0800

    MVP completed

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 let result = document.getElementById('result')
 // Using array.from to convert collection to array
 let buttons = Array.from(document.getElementsByClassName('btn'))
-
+// map through each button and perform calc
 buttons.map((button) => {
   button.addEventListener('click', (e) => {
     switch (e.target.innerText) {
+      case 'C':
+        result.innerText = ' '
+        break
+      // takes the current display and does math operations
+      case '=':
+        try {
+          result.innerText = eval(result.innerText)
+        } catch {
+          result.innerText = 'err'
+        }
+        break
       default:
         result.innerText += e.target.innerText
     }
   })
 })
-
-const clearCount = () => {
-  result.innerHTML = '0'
-}
